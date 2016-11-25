7a501a39d67eca0a4c9aa83b833f409bbfe34a98
Nataniel Freire de Paiva <nataniel.paiva@gmail.com>
2016-11-25 19:49:12 -0200

    s

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var usuario = require('./routes/usuario');
+var perfil = require('./routes/perfil');
 
 var mongoose = require('mongoose');
 
@@ -53,6 +53,7 @@ app.use(function(req, res, next) {
 
 app.use('/', routes);
 app.use('/usuario', usuario);
+app.use('/perfil', perfil);
 
 
 
